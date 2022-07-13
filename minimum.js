ec8feafd8ee8d800687efb0acfacf0ce4249709c
Lorna <lornaroberts99@gmail.com>
2022-07-13 11:05:17 +0100

    Add files via upload

---
+++ minimum.js
@@ -0,0 +1,13 @@
+/* Write a function 'min' that takes two arguments and 
+returns their minimum. */
+
+const min = function(num1, num2){
+if (num1 < num2){
+    console.log(num1);
+}else{
+    console.log(num2);
+}
+
+}
+
+min(3,4);
